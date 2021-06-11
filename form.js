76665453b00211981401d61698a634e2053330ae
Carly Morris <thabai@hotmail.com>
2021-06-11 16:37:30 +0100

    second commit

--- form.js
+++ form.js
@@ -1,57 +1,12 @@
-var myInput = document.getElementById("psw");
-var letter = document.getElementById("letter");
-var capital = document.getElementById("capital");
-var number = document.getElementById("number");
-var length = document.getElementById("length");
+//password matching alert
 
-
-myInput.onfocus = function () {
-  document.getElementById("message").style.display = "block";
-}
-
-
-myInput.onblur = function () {
-  document.getElementById("message").style.display = "none";
-}
-
-
-myInput.onkeyup = function () {
-  // Validate lowercase letters
-  var lowerCaseLetters = /[a-z]/g;
-  if (myInput.value.match(lowerCaseLetters)) {
-    letter.classList.remove("invalid");
-    letter.classList.add("valid");
-  } else {
-    letter.classList.remove("valid");
-    letter.classList.add("invalid");
+var passwordMatch = function() {
+    if (document.getElementById('psw').value ==
+      document.getElementById('pswRepeat').value) {
+      document.getElementById('message').style.color = 'green';
+      document.getElementById('message').innerHTML = 'Passwords match';
+    } else {
+      document.getElementById('message').style.color = 'red';
+      document.getElementById('message').innerHTML = 'Passwords do not match';
+    }
   }
-
-  // Validate capital letters
-  var upperCaseLetters = /[A-Z]/g;
-  if (myInput.value.match(upperCaseLetters)) {
-    capital.classList.remove("invalid");
-    capital.classList.add("valid");
-  } else {
-    capital.classList.remove("valid");
-    capital.classList.add("invalid");
-  }
-
-  // Validate numbers
-  var numbers = /[0-9]/g;
-  if (myInput.value.match(numbers)) {
-    number.classList.remove("invalid");
-    number.classList.add("valid");
-  } else {
-    number.classList.remove("valid");
-    number.classList.add("invalid");
-  }
-
-  // Validate length
-  if (myInput.value.length >= 8) {
-    length.classList.remove("invalid");
-    length.classList.add("valid");
-  } else {
-    length.classList.remove("valid");
-    length.classList.add("invalid");
-  }
-}
